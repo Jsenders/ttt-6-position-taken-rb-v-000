e9cdce208e7ba00c0622d04279c21ebebcd02bb0
Joseph Senders <jasenders@gmail.com>
2018-05-25 18:00:06 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
     return false
   elsif board[index] == ""
     return false
-  elsif board[index] == "nil"
+  elsif board[index] == nil
     return false
   elsif board[index] == "X" || board[index] == "O"
     return true
