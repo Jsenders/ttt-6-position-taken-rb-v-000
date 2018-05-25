992fdc245fa0b691e2345e3b1ff28b6975f5bd7b
Joseph Senders <jasenders@gmail.com>
2018-05-25 17:30:50 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,11 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
+  if index == (" ")
+    puts "false"
+  else index == ("")
+    puts "false"
+  elsif index == "nil"
+    puts "false"
+  elsif index == ("X") || ("O")
+    puts "true"
 end
