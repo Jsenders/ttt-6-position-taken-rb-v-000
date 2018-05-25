1abd7ddf01958a7d7fe340d021dfba08400af71d
Joseph Senders <jasenders@gmail.com>
2018-05-25 17:32:06 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,11 @@
 # code your #position_taken? method here!
-def position_taken?(board, index)
+def position_taken?(board, index = "nil")
   if index == (" ")
     puts "false"
   else index == ("")
     puts "false"
   elsif index == "nil"
     puts "false"
-  elsif index == ("X") || ("O")
+  elsif index == "X" || "O"
     puts "true"
 end
